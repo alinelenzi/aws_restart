d7f3de8ab59bbe7ffb1ecc36c8c9d5c909ef9a36
Aline Lenzi <142021366+alinelenzi@users.noreply.github.com>
2024-05-20 18:33:31 -0300

    Add files via upload

---
+++ tiposNumericos.py
@@ -0,0 +1,38 @@
+adding = 2+2;
+print(adding);
+
+subtracting = 4-2;
+print(subtracting);
+
+multiplying = 2*2;
+print(multiplying);
+
+dividing = 4/2;
+print(dividing)
+
+print("Python has three numeric types: int, float, and complex")
+print("")
+myValue = 1;
+print(myValue);
+print(type(myValue));
+print(str(myValue) + " is of the data type " + str(type(myValue)));
+print("")
+myValue1 = 3.14;
+print(myValue1);
+print(type(myValue1));
+print(str(myValue1) + " is of the data type " + str(type(myValue1)));
+print("")
+myValue2 = 5j;
+print(myValue2);
+print(type(myValue2));
+print(str(myValue2) + " is of the data type " + str(type(myValue2)));
+print("")
+myValue3 = True;
+print(myValue3);
+print(type(myValue3));
+print(str(myValue3) + " is of the data type " + str(type(myValue3)));
+print("")
+myValue4 = False;
+print(myValue4);
+print(type(myValue4));
+print(str(myValue4) + " is of the data type " + str(type(myValue4)));
